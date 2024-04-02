ac70aaafdc822a5fea6f37d326ae21c624ad6e97
oottaabbeekk <newcoder000@.gmail.com>
2024-04-02 23:29:19 +0500

    Beeline

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// let btn = document.querySelector('.div');
+// let div = document.querySelector('.javascript');
+// let x = document.querySelector('.x')
+
+// btn.addEventListener('click', () => {
+//     div.classList.add('show')
+// })
+
+// x.addEventListener('click', () => {
+//     div.classList.remove('show')
+// })
+
+
+let btn = document.querySelector('.plar');
+let div = document.querySelector('.nol');
+let x = document.querySelector('.x')
+
+btn.addEventListener('click', () => {
+    div.classList.add('show')
+})
+
+x.addEventListener('click', () => {
+    div.classList.remove('show')
+})
